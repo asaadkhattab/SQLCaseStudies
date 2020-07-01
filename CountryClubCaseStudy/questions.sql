3abb8af25f5c4692eeff2ffd5164eb4636c7c745
asaadkhattab <asaad@khattab.live>
2020-07-01 16:05:28 -0400

    question3

--- CountryClubCaseStudy/questions.sql
+++ CountryClubCaseStudy/questions.sql
@@ -40,22 +40,26 @@ Write a SQL query to produce a list of the names of the facilities that do. */
 
 
 SELECT name, membercost
-FROM `Facilities`
-WHERE membercost > '0.00'
-LIMIT 0 , 30
+	FROM `Facilities`
+	WHERE membercost > '0.00'
+	LIMIT 0 , 30
 
 
 /* Q2: How many facilities do not charge a fee to members? */
 SELECT COUNT(*) AS free_membership
-FROM `Facilities`
-WHERE membercost = '0.0'
+	FROM `Facilities`
+	WHERE membercost = '0.0'
 
 
 /* Q3: Write an SQL query to show a list of facilities that charge a fee to members,
 where the fee is less than 20% of the facility's monthly maintenance cost.
 Return the facid, facility name, member cost, and monthly maintenance of the
 facilities in question. */
-
+SELECT facid, name, membercost, monthlymaintenance
+	FROM Facilities
+	WHERE membercost >0
+	AND membercost < 0.2 * monthlymaintenance
+LIMIT 0 , 30
 
 /* Q4: Write an SQL query to retrieve the details of facilities with ID 1 and 5.
 Try writing the query without using the OR operator. */
